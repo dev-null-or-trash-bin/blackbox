e80d7f3213d23dfe6f24d6ece45e47fce584c308
dev-null-or-trash-bin <dev-null-or-trash-bin@gmail.com>
2014-02-06 20:24:31 +0100

    initial commit

---
+++ src/Via/Bundle/CoreBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace Via\Bundle\CoreBundle\Entity;
+
+use Sonata\UserBundle\Entity\BaseUser as BaseUser;
+use Doctrine\ORM\Mapping as Orm;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="Group")
+     * @ORM\JoinTable(name="user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
