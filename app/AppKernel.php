65cff416b76c6f3ce626f1dad13fbabb117ea9f2
dev-null-or-trash-bin <dev-null-or-trash-bin@gmail.com>
2014-02-08 18:10:20 +0100

    revert

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,8 +33,6 @@ class AppKernel extends Kernel
             // Then add SonataAdminBundle
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Via\Bundle\CoreBundle\ViaCoreBundle(),
-            new Via\Bundle\ProductBundle\ViaProductBundle(),
-            new Via\Bundle\VariableProductBundle\ViaVariableProductBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Via/Bundle/CoreBundle/Entity/ProductInterface.php
+++ src/Via/Bundle/CoreBundle/Entity/ProductInterface.php
@@ -1,49 +1,7 @@
 <?php
-namespace Via\Bundle\CoreBundle\Entity;
+namespace Via\Bundle\ProductBundle\Entity;
 
 interface ProductInterface
 {
-   
-    /**
-     * Gets product price.
-     *
-     * @return integer $price
-     */
-    public function getPrice();
-    
-    /**
-     * Sets product price.
-     *
-     * @param float $price
-    */
-    public function setPrice($price);
-    
-    /**
-     * Get product short description.
-     *
-     * @return string
-    */
-    public function getShortDescription();
-    
-    /**
-     * Set product short description.
-     *
-     * @param string $shortDescription
-    */
-    public function setShortDescription($shortDescription);
-    
-    /**
-     * Get all product images.
-     *
-     * @return Collection
-     */
-    public function getImages();
-    
-    /**
-     * Get product main image.
-     *
-     * @return ImageInterface
-    */
-    public function getImage();
 
 }
