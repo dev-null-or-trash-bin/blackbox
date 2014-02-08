068f2986811d483ea66d10d443447d9a6224ef30
dev-null-or-trash-bin <dev-null-or-trash-bin@gmail.com>
2014-02-08 20:19:14 +0100

    remove Group Entity

--- src/Via/Bundle/CoreBundle/Entity/User.php
+++ src/Via/Bundle/CoreBundle/Entity/User.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as Orm;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * @ORM\Table(name="user")
  */
 class User extends BaseUser
 {
@@ -16,15 +16,6 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
-    /**
-     * @ORM\ManyToMany(targetEntity="Group")
-     * @ORM\JoinTable(name="user_group",
-     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
-     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
-     * )
-     */
-    protected $groups;
     
     public function __construct()
     {
